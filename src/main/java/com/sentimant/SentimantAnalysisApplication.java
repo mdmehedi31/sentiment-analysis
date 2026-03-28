dfcc53f5ed2ace937d6def5227e4c4fd7b287d8c
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-03-28 21:45:13 +0600

    init commit

---
+++ src/main/java/com/sentimant/SentimantAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.sentimant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimantAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimantAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sentimant/SentimantAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentimant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimantAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
