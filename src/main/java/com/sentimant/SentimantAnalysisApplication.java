d0abd590eadaafde3f458119d56926ec2b9f9096
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-03-29 09:38:30 +0600

    config set up for open AI

--- src/main/java/com/sentimant/SentimantAnalysisApplication.java
+++ src/main/java/com/sentimant/SentimantAnalysisApplication.java
@@ -2,12 +2,14 @@ package com.sentimant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-public class SentimantAnalysisApplication {
+@StyleSheet("styles.css")
+public class SentimantAnalysisApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(SentimantAnalysisApplication.class, args);
     }
-
 }
